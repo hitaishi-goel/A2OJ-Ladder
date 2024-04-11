ccb34e01bb3c0677a5aea24a622531b3fa8352fe
Hitaishi Goel <124267268+hitaishi-goel@users.noreply.github.com>
2024-04-11 19:52:28 +0530

    Added Third solution

--- 1300_below/q3.cpp
+++ 1300_below/q3.cpp
@@ -6,6 +6,7 @@ using namespace std;
 #define mp make_pair
 #define ff first
 #define ss second
+#define make_arr cin>>arr[i];
 #define endl '\n'
 #define all(x) x.begin(), x.end()
 #define rall(x) x.rbegin(), x.rend()
@@ -21,5 +22,18 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+    int n,t;
+    cin>>n>>t;
+    string s(n,'0');
+    for(int i=0;i<n;i++)cin>>s[i];
+    while(t--){
+        for(int i=0;i<n-1;){
+            if(s[i]=='B' && s[i+1]=='G'){swap(s[i],s[i+1]);
+            i+=2;
+            }
+            else i++;
+        }
+    }
+    for(int i=0;i<n;i++)cout<<s[i];
     return 0;
 } 
